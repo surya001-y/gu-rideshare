917bbad165c7bde374a64aa9706de1385ce61176
Suryansh Yadav <suryansh9366@gmail.com>
2026-08-27 00:29:54 +0530

    Fix production API URL

--- gu-rideshare/src/api.js
+++ gu-rideshare/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://gu-rideshare-backend.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
